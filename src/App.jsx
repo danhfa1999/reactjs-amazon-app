96df3a2336f71d2265216d877a87f7f104e51014
Henry <48523889+danhfa1999@users.noreply.github.com>
2021-09-17 21:04:45 +0700

    Add page checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import "./App.css";
-import Header from "./Header";
-import Home from "./Home";
+import Header from "./Component/Header/Header";
+import Home from "./Component/Home/Home";
+import Checkout from "./Component/Checkout/Checkout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
